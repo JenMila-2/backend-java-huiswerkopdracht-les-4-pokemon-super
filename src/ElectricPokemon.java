03cc570c45fc55ec44a2930d596b77f3c435a2c2
jensyfranco <jenmilafran@gmail.com>
2023-03-15 16:12:27 +0100

    Huiswerkopdracht les 4 Pokemon Super

--- src/ElectricPokemon.java
+++ src/ElectricPokemon.java
@@ -2,60 +2,6 @@ import java.util.ArrayList;
 
 public class ElectricPokemon extends PokemonSuper {
 
-    //*** Additional Variables ***//
-    private String category;
-    private String electricPokemonWeakness;
 
-
-    //*** Parameterized Constructor ***//
-    public ElectricPokemon(String name, String type, double weight, double height, double level, double hp, String category, String sound, String food, String electricPokemonWeakness) {
-        super(name, type, weight, height, level, hp, sound, food);
-        this.category = category;
-        this.electricPokemonWeakness = electricPokemonWeakness;
-    }
-
-
-    //*** Getters and Setters ***//
-    public String getCategory() {
-
-        return this.category;
-    }
-    public void setCategory(String category) {
-
-        this.category = category;
-    }
-
-    public String getElectricPokemonWeakness() {
-
-        return this.electricPokemonWeakness;
-    }
-    public void setElectricPokemonWeakness(String electricPokemonWeakness) {
-
-        this.electricPokemonWeakness = electricPokemonWeakness;
-    }
-
-
-    //*** Methods ***//
-    public void electroBall() {
-        System.out.println("Come on " + getName() + ", you can do it! Use Electro Ball!");
-    }
-    //Electro Ball | power: n/a, accuracy: 100%, PP count: 10 (max 16)
-
-    public void voltTackle() {
-        System.out.println("Volt Tackle!!!" + " Do it now " + getName()+ "!");
-    }
-    //Volt Tackle | power: 120, accuracy: 100%, PP count: 15 (max 24)
-    //Volt Tackle deals damage and has a 10% chance of paralyzing the target, but the user receives 1⁄3 of the damage it inflicted in recoil. In other words, if the attack does 90 HP damage to the opponent, the user will lose 30 HP.
-
-
-    //*** Abstract Methods - From the PokemonSuper class ***//
-    @Override
-    public void speaks() {
-        System.out.println(getName() + " let yourself be heard! >>> " + getSound());
-    }
-    @Override
-    public void mainWeaknesses() {
-        System.out.println("Electric Pokemon " + getName() + " weaknesses are: ground.");
-    }
 
 }
--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -2,63 +2,6 @@ import java.util.ArrayList;
 
 public class FirePokemon extends PokemonSuper {
 
-    //*** Additional Variables ***//
-    private String category;
-    private String firePokemonWeakness;
 
-
-    //*** Parameterized Constructor ***//
-    public FirePokemon(String name, String type, double weight, double height, double level, double hp, String category, String sound, String food, String firePokemonWeakness) {
-        super(name, type, weight, height, level, hp, sound, food);
-        this.category = category;
-        this.firePokemonWeakness = firePokemonWeakness;
-    }
-
-
-    //*** Getters and Setters ***//
-    public String getCategory() {
-
-        return this.category;
-    }
-    public void setCategory(String category) {
-
-        this.category = category;
-    }
-
-    public String getFirePokemonWeakness() {
-
-        return this.firePokemonWeakness;
-    }
-    public void setFirePokemonWeakness(String firePokemonWeakness) {
-
-        this.firePokemonWeakness = firePokemonWeakness;
-    }
-
-
-    //*** Methods ***//
-    public void searingShot() {
-        System.out.println(getName() + " use your Searing Shot!");
-    }
-    //Searing Shot | power: 100, accuracy: 100%, PP count: 5
-    //Searing Shot is a special attack
-    //Searing Shot has a chance of burning the opponent
-
-    public void fireLash() {
-        System.out.println("Use your Fire Lash " + getName() + "!");
-    }
-    //Fire Lash | power: 80, accuracy: 100%, PP count: 15
-    //Fire Lash is a physical attack
-    //Fire Lash lowers the opponent's Defense
-
-
-    //*** Abstract Methods - From the PokemonSuper class ***//
-    @Override
-    public void speaks() {
-        System.out.println(getName() + " says >>> " + getSound());
-    }
-    @Override
-    public void mainWeaknesses() {
-        System.out.println("Fire Pokemon " + getName() + " weaknesses are: water, ground and rock.");
-    }
 
 }
